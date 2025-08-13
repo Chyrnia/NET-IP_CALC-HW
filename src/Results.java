a7b42cdb1705982070ad1e6ac9d7ec116089a86a
Andres Chyrnia <andreschyrnia@gmail.com>
2025-08-13 19:08:53 -0300

    Add fields to Results, to display everything asked

--- src/Results.java
+++ src/Results.java
@@ -22,8 +22,48 @@ public class Results {
 		return broadcastIp;
 	}
 
+	public String getValidatedMask(){
+		return validatedMask;
+	}
+
+	public String getBinaryIp(){
+		return binaryIp;
+	}
+
+	public String getBinaryMask(){
+		return binaryIp;
+	}
+
+	public String getBinaryNet(){
+		return binaryNet;
+	}
+
+	public String getBinaryBroad(){
+		return binaryBroad;
+	}
+
 	public void setValidatedIp(String validIp){
 		this.validatedIp = validIp;
+	}
+
+	public void setValidatedMask(String validMask){
+		this.validatedMask = validMask;
+	}
+
+	public void setBinaryIp(String binIp){
+		this.binaryIp = binIp;
+	}
+
+	public void setBinaryMask(String binMask){
+		this.binaryMask = binMask;
+	}
+
+	public void setBinaryNet(String binNet){
+		this.binaryNet = binNet;
+	}
+
+	public void setBinaryBroad(String binBroad){
+		this.binaryBroad = binBroad;
 	}
 
 	public void setNetworkIp(String netIp){
@@ -37,7 +77,13 @@ public class Results {
 	
 	//Data fields
 	private String validatedIp;
+	private String validatedMask;
 	private String networkIp;
 	private String broadcastIp;
+
+	private String binaryIp;
+	private String binaryMask;
+	private String binaryNet;
+	private String binaryBroad;
 
 }
