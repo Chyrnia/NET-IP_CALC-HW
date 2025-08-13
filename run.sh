f9633b02531a3332bfe3c3c73b1afc8ba8796c1d
Andres Chyrnia <andreschyrnia@gmail.com>
2025-08-13 19:28:25 -0300

    Fix so the script runs our program

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 function build(){
     make # Step 1: Build the project
     cd bin || exit # Step 2: Change to the bin directory
-    java Main $1 # Step 3: Run the Java program
+    java CalculadoraRed $1 # Step 3: Run the Java program
     cd ..  # Step 4: Return to the previous directory
 }
 
 function run(){
 	cd bin || exit #Change to the bin directory
-	java Main $1 #Run the program
+	java CalculadoraRed $1 #Run the program
 	cd .. #Go back
 }
 
